3997eaf0e3d5b6cc02482f6d2e99bfaa892e04c0
Kim SeongUk <so3659@naver.com>
2022-05-23 20:14:20 +0900

    Update test_code

--- app/app.js
+++ app/app.js
@@ -1,68 +1,35 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParse.json());
+const apiRouter = express.Router();
 
-app.post('/api/sayHello', (req, res) => {
-    const responseBody = { //skill response
-        version: "2.0", //version 필수
-        template: { // template 필수
-            outputs: [
-                {
-                    simpleText: {
-                        text: "hello I'm Ryan"
-                    }
-                }
-            ]
+app.use(logger('dev', {}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/api', apiRouter);
+
+apiRouter.post('/sayHello', function(req, res) {
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: "MBTI 검사 챗봇입니다!"
+          }
         }
-    };
-    res.status(200).send(responseBody);
-});
-
-app.post('/api/showHello', (req, res) => {
-    console.log(req.body);
-    const responseBody = {
-        version: "2.0",
-        template: {
-            outputs: [
-                {
-                    simpleImage: {
-                        imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-                        altText: "hello I'm Ryan"
-                    }
-                }
-            ]
-        }
-    };
-    res.status(200).send(responseBody);
-});
-
-app.post('/message', (req, res) => {
-    const question = req.body.userRequest.utterance;
-    if (question === 'test') {
-        const responseBody = {
-            version: "2.0",
-            template: {
-                outputs: [
-                    {
-                        simpleText: {
-                            text: "text..."
-                        }
-                    }
-                ],
-                quickReplies: [
-                    {
-                        label: 'go main',
-                        action: 'message',
-                        messageText: 'go main'
-                    }
-                ]
-            }
-        };
-        res.status(200).send(responseBody);
+      ]
     }
-})
+  };
 
-var server = app.listen(3000);
+  res.status(200).send(responseBody);
+});
+
+app.listen((process.env.PORT || 3000), function() {
+  console.log('Example skill server listening on port 3000!');
+});
